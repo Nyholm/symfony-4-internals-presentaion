67fc04e5d1ee9bb641b5a6310dbf469fbe043847
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-05-16 10:08:13 +0200

    Adding cache

---
+++ 5-cache/Readme.md
@@ -0,0 +1,4 @@
+# Cache
+
+Lets use a cache system for speeding up performance. We are adding some cache libraries
+and a new cache library
---
+++ 5-cache/public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+use Nyholm\Psr7\Factory\ServerRequestFactory;
+use Nyholm\Psr7\Response;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$request = (new ServerRequestFactory())->createServerRequestFromGlobals();
+$response = new Response();
+
+$middlewares[] = new \App\Middleware\Cache();
+$middlewares[] = new \App\Middleware\Router();
+
+$runner = (new \Relay\RelayBuilder())->newInstance($middlewares);
+$response = $runner($request, $response);
+
+// Send response
+echo $response->getBody();
